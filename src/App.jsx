38a8ddcb5fe2b05525495cf29b42ff0065829a7c
RafaKumala <111202214177@mhs.dinus.ac.id>
2025-10-11 21:47:42 +0700

    menggunakan variant class tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const courses = [
+    {
+      title: "System Administration and IT Infrastructure Services",
+    },
+    {
+      title: "Operating Systems Becoming a Power User",
+    },
+    {
+      title: "The Bits and Bytes of Computer Networking",
+    },
+    {
+      title: "Technical Support Fundamentals",
+    },
+    {
+      title: "How to Succeed at: Writing Applications",
+    },
+    {
+      title: "Medicine Administration for Carers",
+    },
+  ];
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex bg-white shadow-md rounded-md overflow-hidden">
-        {/* Bagian kiri (gambar/placeholder) */}
-        <div className="w-[500px] h-[400px] bg-gray-300 flex items-center justify-center text-gray-600 text-2xl">
-          600 × 500
-        </div>
+    <>
+      <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+        {courses.map((course, index) => (
+          <div key={index} className="shadow rounded-lg overflow-hidden flex flex-col
+          hover:border hover:border-gray-500 hover:shadow-lg hover:scale-105 transition-transform duration-300">
 
-        {/* Bagian kanan (form login) */}
-        <div className="w-[400px] p-8 flex flex-col justify-center">
-          <h2 className="text-center text-4xl font-bold mb-6">LOGIN</h2>
+            {/* Image */}
+            <img
+              src="https://www.placehold.co/200x400"
+              alt={course.title}
+              className="w-full h-44 object-cover"
+            />
 
-          <form className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium mb-1">Email</label>
-              <input
-                type="email"
-                placeholder="Masukkan email"
-                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
+            {/* Content */}
+            <div className="p-4 bg-red-100 flex flex-col justify-between h-full">
+              <h3 className="text-lg font-semibold mb-4">{course.title}</h3>
+
+							<div className="bg-red-50 p-2 rounded-lg">
+                <div className="text-sm text-gray-500 mt-2 flex justify-between">
+                  <span>👥 123 users</span>
+                  <span>⏱ 60</span>
+                </div>
+
+              {/* Author */}
+                <div className="mt-3 mb-2 flex items-center gap-2">
+                  <img
+                    src="https://www.placehold.co/50x50"
+                    alt="Author's Avatar"
+                    className="w-12 h-12 rounded-full object-cover"
+                  />
+                  <div>
+                    <p className="text-sm font-medium">Author's Name</p>
+                    <p className="text-xs text-gray-500">Designer</p>
+                  </div>
+                </div>
+              </div>
+
+            {/* Footer */}
+              <div className="mt-4 flex items-center justify-between">
+                <div className="bg-white py-2 px-4 rounded">$123</div>
+                <button className="bg-red-700 hover:bg-red-900 active:bg-red-500 text-white px-4 py-2 rounded text-sm">
+                  Get Started
+                </button>
+              </div>
             </div>
-
-            <div>
-              <label className="block text-sm font-medium mb-1">Password</label>
-              <input
-                type="password"
-                placeholder="Masukkan password"
-                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-            </div>
-
-            <button
-              type="submit"
-              className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-md transition"
-            >
-              Login
-            </button>
-          </form>
-        </div>
+          </div>  
+        ))}
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
