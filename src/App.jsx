57f3a0acc6d669bed356537c3aeae56eb4860750
RafaKumala <111202214177@mhs.dinus.ac.id>
2025-12-30 01:35:52 +0700

    Update minggu 13

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import SignInPage from "./pages/signIn";
+import SignUpPage from "./pages/signUp";
+import ErrorPage from "./pages/error";
+import DashboardPage from "./pages/dashboard";
+import BalancePage from "./pages/balance.jsx";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+
+function App() {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <DashboardPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/login",
+      element: <SignInPage />,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage />,
+    },
+    {
+      path: "/balance",
+      element: <BalancePage />,
+    }
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+}
+
+export default App;
